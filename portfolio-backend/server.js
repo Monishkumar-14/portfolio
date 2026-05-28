02a28146656bb98e3f20aa31728368f87fc46008
Monishkumar E M <monish1421@gmail.com>
2026-05-28 17:35:13 +0530

    Update CORS origins to allow monishdev.online

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -17,8 +17,8 @@ const ALLOWED_ORIGINS = [
   "http://localhost:5173",
   "http://localhost:4173",
   "http://127.0.0.1:5173",
-  "https://monishdev.in",
-  "https://www.monishdev.in",
+  "https://monishdev.online",
+  "https://www.monishdev.online",
   "https://portfolio-phi-ten-lxkms7co9f.vercel.app",
   process.env.CLIENT_URL,           // still respected if set in .env
 ].filter(Boolean);                  // removes undefined if CLIENT_URL isn't set
